3ebd863bbd48bb2b230bccacc2f50c6e3831fd62
Saidnazar-EPAM <102347678+Saidnazar-EPAM@users.noreply.github.com>
2022-04-15 00:01:03 +0500

    Create red test for FizzBuzz

---
+++ Module11.Test/FizzBuzzTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xunit;
+
+namespace Module11.Test
+{
+    public class FizzBuzzTest
+    {
+        [Fact]
+        public void FizzBuzzing_ReturnsEnumerable()
+        {
+            var fizzBuzz = new FizzBuzz();
+            var i = 0;
+            foreach (var item in fizzBuzz.FizzBuzzing())
+            {
+                i++;
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Assert.Equal("FizzBuzz", item);
+                    continue;
+                }
+                if (i % 3 == 0)
+                {
+                    Assert.Equal("Fizz", item);
+                    continue;
+                }
+                if (i % 5 == 0)
+                {
+                    Assert.Equal("Buzz", item);
+                    continue;
+                }
+                Assert.Equal(i.ToString(), item);
+            }
+        }
+
+    }
+}
---
+++ Module11/FizzBuzz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module11
+{
+    public class FizzBuzz
+    {
+        public IEnumerable<string> FizzBuzzing()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
